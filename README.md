2007b0ae64cc2140d7a26be43bd2b4559a865b63
Kaushikdhola <kaushikdhola02@gmail.com>
2024-03-11 19:53:08 -0300

    update error handling

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# T5Web
+# Tutorial 5
+
+- **Date Created:** February 17, 2024
+- **Last Modification Date:** February 17, 2024
+- **Deployed URL:** [Tutorial 5 Deployment](https://t5web.onrender.com/)
+- **Git URL:** [Git Repository](https://git.cs.dal.ca/kdhola/csci-5709-tutorials/-/tree/main/Tutorial4)
+
+## Author
+- **Name:** Kaushik Chanabhai Dhola
+- **GitLab Profile:** [Kaushik Chanabhai Dhola](https://git.cs.dal.ca/kdhola)
+- **Banner ID:** B00956419
+
+## Built With
+- **Angular:** The web framework used
+- **npm:** Dependency Management
--- app.py
+++ app.py
@@ -2,19 +2,6 @@ from flask import Flask, jsonify, request
 import json
 app = Flask(__name__)
 
-users = [
-    {
-        "email": "abc@abc.ca",
-        "firstName": "ABC",
-        "id": "5abf6783"
-    },
-    {
-        "email": "xyz@xyz.ca",
-        "firstName": "XYZ",
-        "id": "5abf674563"
-    }
-]
-
 file_path = "users.json"
 
 def read_users():
@@ -31,52 +18,69 @@ def write_users(users):
         print(users)
         json.dump(users, file, indent=4)
 
+@app.route('/')
+def hello():
+    return "hellow, please use /users, /user, /add, /update to test the application."
+
 @app.route('/users', methods=['GET'])
 def get_users():
-    users = read_users()
-    return jsonify({
-        "message": "Users retrieved",
-        "success": True,
-        "users": users
-    })
+    try:
+        users = read_users()
+        return jsonify({
+            "message": "Users retrieved",
+            "success": True,
+            "users": users
+        }), 200
+    except Exception as e:
+        return jsonify({"message": "Failed to get users", "success": False, "error": str(e)}), 500
+
 
 @app.route('/add', methods=['POST'])
 def add_user():
-    data = request.json
-    if 'email' not in data or 'firstName' not in data:
-        return jsonify({"message": "Missing data in request body", "success": False}), 400
-    users = read_users()
-    new_user = {
-        "email": data['email'],
-        "firstName": data['firstName'],
-        "id": str(len(users) + 1)
-    }
-    users.append(new_user)
-    write_users(users)
+    try:
+        data = request.json
+        if 'email' not in data or 'firstName' not in data:
+            return jsonify({"message": "Missing data in request body", "success": False}), 400
+        users = read_users()
+        new_user = {
+            "email": data['email'],
+            "firstName": data['firstName'],
+            "id": str(len(users) + 1)
+        }
+        users.append(new_user)
+        write_users(users)
 
-    return jsonify({"message": "User added", "success": True}), 201
+        return jsonify({"message": "User added", "success": True}), 201
+    except Exception as e:
+        return jsonify({"message": "Failed to add user", "success": False, "error": str(e)}), 500
 
 @app.route('/update/<string:id>', methods=['PUT'])
 def update_user(id):
-    data = request.json
-    users=read_users()
-    for user in users:
-        if user['id'] == id:
-            user['email'] = data.get('email', user['email'])
-            user['firstName'] = data.get('firstName', user['firstName'])
-            write_users(users)
-            return jsonify({"message": "User updated", "success": True}), 200
-    
-    return jsonify({"message": "User not available", "success": False}), 404
+    try:
+        data = request.json
+        users=read_users()
+        for user in users:
+            if user['id'] == id:
+                user['email'] = data.get('email', user['email'])
+                user['firstName'] = data.get('firstName', user['firstName'])
+                write_users(users)
+                return jsonify({"message": "User updated", "success": True}), 200
+        
+        return jsonify({"message": "User not available", "success": False}), 404
+    except Exception as e:
+        return jsonify({"message": "Failed to update user", "success": False, "error": str(e)}), 500
 
 @app.route('/user/<string:id>', methods=['GET'])
 def get_user(id):
-    users = read_users()
-    for user in users:
-        if user['id'] == id:
-            return jsonify({"success": True, "user": user}), 200
-    
-    return jsonify({"message": "User not available", "success": False}), 404
+    try:
+        users = read_users()
+        for user in users:
+            if user['id'] == id:
+                return jsonify({"success": True, "user": user}), 200
+        
+        return jsonify({"message": "User not available", "success": False}), 404
+    except Exception as e:
+        return jsonify({"message": "Failed to retrieve user", "success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
