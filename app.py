081eee09ae6324a1b37c5030f2be642561b87221
Kaushikdhola <kaushikdhola02@gmail.com>
2024-03-11 17:03:47 -0300

    add json file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-
+import json
 app = Flask(__name__)
 
 users = [
@@ -15,8 +15,25 @@ users = [
     }
 ]
 
+file_path = "users.json"
+
+def read_users():
+    
+    try:
+        with open(file_path, "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        users = []
+    return users
+
+def write_users(users):
+    with open(file_path, "w") as file:
+        print(users)
+        json.dump(users, file, indent=4)
+
 @app.route('/users', methods=['GET'])
 def get_users():
+    users = read_users()
     return jsonify({
         "message": "Users retrieved",
         "success": True,
@@ -28,28 +45,33 @@ def add_user():
     data = request.json
     if 'email' not in data or 'firstName' not in data:
         return jsonify({"message": "Missing data in request body", "success": False}), 400
-    
+    users = read_users()
     new_user = {
         "email": data['email'],
         "firstName": data['firstName'],
         "id": str(len(users) + 1)
     }
     users.append(new_user)
+    write_users(users)
+
     return jsonify({"message": "User added", "success": True}), 201
 
 @app.route('/update/<string:id>', methods=['PUT'])
 def update_user(id):
     data = request.json
+    users=read_users()
     for user in users:
         if user['id'] == id:
             user['email'] = data.get('email', user['email'])
             user['firstName'] = data.get('firstName', user['firstName'])
+            write_users(users)
             return jsonify({"message": "User updated", "success": True}), 200
     
     return jsonify({"message": "User not available", "success": False}), 404
 
 @app.route('/user/<string:id>', methods=['GET'])
 def get_user(id):
+    users = read_users()
     for user in users:
         if user['id'] == id:
             return jsonify({"success": True, "user": user}), 200
